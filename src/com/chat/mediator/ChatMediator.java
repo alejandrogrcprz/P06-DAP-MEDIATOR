89a80c6e8b1c2728ae92c0d03068a7d3665204d4
Alejandro García Pérez <alu0101441207@ull.edu.es>
2025-11-20 16:47:20 +0000

    Añadida interfaz ChatMediator: Define el contrato de comunicación

--- src/com/chat/mediator/ChatMediator.java
+++ src/com/chat/mediator/ChatMediator.java
@@ -1,4 +1,11 @@
 package com.chat.mediator;
 
-public class ChatMediator {
+import com.chat.user.User;
+
+public interface ChatMediator {
+  void sendMessage(String msg, User user); // Enviar a grupo
+  void sendPrivateMessage(String msg, User sender, String receiverName); // Enviar privado
+  void addUser(User user);
 }
+
+
