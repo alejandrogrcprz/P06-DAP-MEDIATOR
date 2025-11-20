0f886811ccb274f8474c505cf28f4ef50750c911
Alejandro García Pérez <alu0101441207@ull.edu.es>
2025-11-20 16:47:50 +0000

    Implementación de ChatRoom: Lógica de enrutamiento de mensajes grupales y privados

--- src/com/chat/mediator/ChatRoom.java
+++ src/com/chat/mediator/ChatRoom.java
@@ -1,4 +1,43 @@
 package com.chat.mediator;
 
-public class ChatUser {
+import com.chat.user.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatRoom implements ChatMediator {
+  private List<User> users;
+
+  public ChatRoom(){
+    this.users = new ArrayList<>();
+  }
+
+  @Override
+  public void addUser(User user){
+    this.users.add(user);
+  }
+
+  @Override
+  public void sendMessage(String msg, User user) {
+    for(User u : this.users){
+      // No enviar el mensaje a quien lo escribió
+      if(u != user){
+        u.receive(msg, user.getName(), false);
+      }
+    }
+  }
+
+  @Override
+  public void sendPrivateMessage(String msg, User sender, String receiverName) {
+    boolean found = false;
+    for(User u : users){
+      // Comparamos nombres ignorando mayúsculas/minúsculas
+      if(u.getName().equalsIgnoreCase(receiverName)){
+        u.receive(msg, sender.getName(), true);
+        found = true;
+      }
+    }
+    if(!found){
+      sender.receive("Error: El usuario '" + receiverName + "' no existe o no está conectado.", "SISTEMA", true);
+    }
+  }
 }
