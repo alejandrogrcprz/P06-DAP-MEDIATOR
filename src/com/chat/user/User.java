c518e08d8616804bc08b068df1f3c2e32d279837
Alejandro García Pérez <alu0101441207@ull.edu.es>
2025-11-20 16:47:32 +0000

    Añadida clase abstracta User: Define la estructura base de los participantes

--- src/com/chat/user/User.java
+++ src/com/chat/user/User.java
@@ -1,4 +1,22 @@
 package com.chat.user;
 
-public class User {
+import com.chat.mediator.ChatMediator;
+
+public abstract class User {
+  protected ChatMediator mediator;
+  protected String name;
+
+  public User(ChatMediator med, String name){
+    this.mediator = med;
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  // Métodos que la interfaz gráfica deberá implementar
+  public abstract void send(String msg);
+  public abstract void sendPrivate(String msg, String receiverName);
+  public abstract void receive(String msg, String senderName, boolean isPrivate);
 }
